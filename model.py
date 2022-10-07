a4ab2544e830c8db7078f91228a7736b700adfca
Mark Owusu <markowusuayim@gmail.com>
2022-10-07 11:25:04 +0100

    added: ML pipeline

--- model.py
+++ model.py
@@ -3,11 +3,23 @@ import pickle
 import pandas as pd
 import joblib
 import numpy as np
+import re 
+import nltk 
+from nltk.corpus import stopwords 
+import re
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+import pandas as pd 
+import numpy as np
+from sklearn.feature_extraction.text  import CountVectorizer 
 from prophet import Prophet
 pd.set_option("display.float_format",lambda x: '%.f' % x)
 
 TODAY = datetime.date.today()
 
+pstem = PorterStemmer()
+lem = WordNetLemmatizer()
+stop_words = stopwords.words('english')
 
 #training our model
 # def train():
@@ -40,7 +52,7 @@ def convert_confirmed(prediction_list):
     output={}
     for df in prediction_list:
         date = df["ds"].strftime("%m/%d/%Y")
-        output[date] = df["yhat"]
+        output[date] = df["yhat"] + 54000000
     return output
 
 
@@ -64,9 +76,42 @@ def convert_death(prediction_list):
 
 
 #  TODO: create a pipeline for the model and store in the cache in a Joblib format 
-def sentiment_analysis():
+def sentiment_analysis(data):
     model = joblib.load(open('utils/sentiment_analysis.joblib','rb'))
-    output = model.predict()
-         
-    return output
+    
+    
+
+    cleaned_sentence = " "
+    cleaned_text =  re.sub('https?://[A-Za-z0-9./]+','',data)
+    # remove hash symbols from the dataset 
+    cleaned_text = re.sub("[^a-zA-Z]"," ",cleaned_text)
+
+            #removing RT tags in tweets
+    cleaned_text = re.sub(r'^[RT]+',' ',cleaned_text)
+
+    cleaned_text = re.sub("CDC","center disease control prevention", cleaned_text)
+    cleaned_text = re.sub("WHO","world health organization", cleaned_text)
+
+
+
+            #covert to lower case 
+    cleaned_text = cleaned_text.lower()
+            # removing stop words from the dataset 
+
+    tokens = nltk.word_tokenize(cleaned_text)
+
+    data_output = cleaned_sentence.join((word for word in tokens if word not in stop_words))
+    # print(data_output)
+
+    # cleaned_sentence = " "
+
+    vector = CountVectorizer()
+    converted_data  = vector.fit_transform(np.array(data_output).ravel()).toarray()
+    print(converted_data,"element")
+  # dispaly(converted_data,"This is the data to be vectorized")
+    p = model.predict(converted_data)
+    target = ["neutral","positive","negative"]    
+    # print(target[p[0]],"amazing")
+
+    return {"message":p}
         
--- utils/sentiment_analysis.py
+++ utils/sentiment_analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 from nltk.corpus import stopwords
 import numpy as np
 import nltk 
+from sklearn.base import TransformerMixin, BaseEstimator
 from nltk.corpus import stopwords 
 import re
 from nltk.stem import WordNetLemmatizer
@@ -67,37 +68,85 @@ def cleanDataset(rows=1,all_rows = False):
     return cleaned_sentence.join((word for word in tokens if word not in stop_words)) 
     
 
-cleanDataset(all_rows=True)
+cleanDataset(rows=300, all_rows=False)
 
 
 
-def get_vector_transformation(cleaned_data):
-    vector = CountVectorizer()
-    converted_data  = vector.fit_transform(cleaned_data).todense()
-    return converted_data
+class get_vector_transformation(TransformerMixin, BaseEstimator):
+
+    def __init__(self, cleaned_data):
+        self.cleaned_data = cleaned_data
+
+    def fit(self, x, y =None):
+        return self 
+
+    def transform(self,x,y = None):  
+        transform_data = np.array(self.cleaned_data.loc[:,"cleaned_tweet"]).ravel()
+        vector = CountVectorizer()
+        converted_data  = vector.fit_transform(transform_data).todense()
+        return converted_data
     
 
 covid_dataframe["label"].replace("neu",0,inplace=True)
 covid_dataframe["label"].replace("neg",-1,inplace=True)
 covid_dataframe["label"].replace("pos",1,inplace=True)
 
-X= get_vector_transformation(np.array(covid_dataframe.loc[:,"cleaned_tweet"]).ravel()) # getting the column 
+X= get_vector_transformation(covid_dataframe) # getting the column 
 # X = countVector.transform(np.array(covid_dataframe.loc[:,"cleaned_tweet"]).ravel())   # vector space for text 
 # labelCountVector = get_vector_transformation(np.array(data.loc[:,"label"]).ravel()) 
 # Y = labelCountVector.transform(np.array(covid_dataframe.loc[:,"label"]).ravel()) # vector space for the label data
 Y = np.array(covid_dataframe.iloc[:,1]).ravel()
 
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y, train_size=0.8, random_state=42)
-# I used the random_state to be "42" because it will yeild constants factor of randomness and not madness
-# perfect number, {the answer to life, the universe and everything}. Hitchhiker's guide to life, the universe and everything 
+# # I used the random_state to be "42" because it will yeild constants factor of randomness and not madness
+# # perfect number, {the answer to life, the universe and everything}. Hitchhiker's guide to life, the universe and everything 
 
 
 
-xgb_Classifier = xgb.XGBClassifier()
-xgb_Classifier.fit(X_train,Y_train)
+# xgb_Classifier = xgb.XGBClassifier()
+# xgb_Classifier.fit(X_train,Y_train)
 
-print("Dumping the file in a joblib file ")
+# print("Dumping the file in a joblib file ")
+# import joblib
+
+# model_file = "sentiment_analysis.joblib"
+# sentiment_analysis_file = joblib.dump(xgb_Classifier,model_file)
+
+
 import joblib
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import make_pipeline
 
-model_file = "sentiment_analysis.joblib"
-sentiment_analysis_file = joblib.dump(xgb_Classifier,model_file)
+# Load some categories of newsgroups dataset
+categories = [
+    "soc.religion.christian",
+    "talk.religion.misc",
+    "comp.sys.mac.hardware",
+    "sci.crypt",
+]
+
+sentiment_category = [ "positive","neutral","negative"]
+newsgroups_training = fetch_20newsgroups(
+    subset="train", categories=categories, random_state=0
+)
+newsgroups_testing = fetch_20newsgroups(
+    subset="test", categories=categories, random_state=0
+)
+
+# Make the pipeline
+model = make_pipeline(
+    cleanDataset(rows=300),
+    get_vector_transformation(),
+    # TfidfVectorizer(),
+    MultinomialNB(),
+)
+
+# Train the model
+model.fit(X_train, Y_train)
+
+# Serialize the model and the target names
+model_file = "newsgroups_model.joblib"
+model_targets_tuple = (model,sentiment_category )
+joblib.dump(model_targets_tuple, model_file)
